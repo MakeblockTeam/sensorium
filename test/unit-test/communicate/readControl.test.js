869299fef0ba170858ec08c29a2676a9ab9717e6
unknown <jeremy.yang@makeblock.com>
2017-06-29 00:06:51 +0800

    增加测试用例

--- test/unit-test/communicate/readControl.test.js
+++ test/unit-test/communicate/readControl.test.js
@@ -35,76 +35,99 @@ const UltrasonicProtocol = [0xff, 0x55, 0x04, 0x00, 0x01, 0x01, 0x06];
 describe('test doParse', function() {
   //模拟发送 1 条请求
   it('should create 1 readRecord', function(done) {
+    let CaseReadNumber = 1;
     resetReadControlForTest();
     Cammand.execRead(UltrasonicProtocol, function(val){
       let count = Object.keys(ReadControl.readRecord).length;
-      expect(count).to.eql(1);
+      expect(count).to.eql(CaseReadNumber);
       done();
     }); //ff 55 04 00 01 01 06
   });
 
   //模拟快速创建 25 条请求
-  it('should create 25 readRecords', function(done) {
+  it(`should create 25 readRecords`, function(done) {
+    let CaseReadNumber = 25;
     resetReadControlForTest();
-    let emitFunc = throttler(25);
-    for(let i = 0; i < 25; i++){
+    let emitFunc = throttler(CaseReadNumber);
+    let count = 0;
+    for(let i = 0; i < CaseReadNumber; i++){
       Cammand.execRead(UltrasonicProtocol, function(val){
+        count++;
         emitFunc(function(){
-          // console.log(ReadControl.readRecord);
+          expect(count).to.eql(CaseReadNumber);
           done();
         });
       });
     }
   });
 
-  //模拟快速创建 256 条请求
-  it('should create 256 readRecords but send 255 in fact', function() {
+  //模拟快速创建 255 条请求
+  //预期能执行 255 次请求回调
+  it('快速创建 255 条请求, 预期执行 255 次请求回调', function(done) {
     resetReadControlForTest();
-    let emitFunc = throttler(256);
-    for(let i = 0; i < 256; i++){
+    let CaseReadNumber = 255;
+    let emitFunc = throttler(CaseReadNumber);
+    let count = 0;
+    for(let i = 0; i < CaseReadNumber; i++){
       Cammand.execRead(UltrasonicProtocol, function(val){
+        count++;
         emitFunc(function(){
-          // console.log(Object.keys(ReadControl.readRecord).length);
+          expect(count).to.eql(CaseReadNumber);
           done();
         });
       });
     }
   });
 
-  // //模拟匀速(10ms/条)创建 255 条请求
-  it('should create 255 readRecords and successfully handled them', function() {
+  // 模拟匀速(20ms/条)创建 255 条请求
+  // 预期执行 255 次请求回调
+  it('匀速(20ms/条)创建 255 条请求，预期执行不超过 255 次请求回调', function(done) {
     resetReadControlForTest();
+    this.timeout(6000);
+    let CaseReadNumber = 255;
+    let isDone = false;
+    let count = 0; //回调计数
     let i = 0;
-    let emitFunc = throttler(255);
     let timer = setInterval(function(){
-      if(i++ < 255){
+      if(i++ < CaseReadNumber){
         Cammand.execRead(UltrasonicProtocol, function(val){
-          emitFunc(function(){
+          if(++count == CaseReadNumber){
+            clearInterval(timer);
+            expect(count).to.eql(CaseReadNumber);
             done();
-          });
+          };
         });
       }else{
         clearInterval(timer);
+        expect(count).to.most(CaseReadNumber);
+        done();
       }
-    }, 10);
+    }, 20);
   });
 
-  //模拟匀速(10ms/条)创建 256 条请求
-  it('should create 256 readRecords and some of them will be ignored', function() {
+  //模拟匀速(60ms/条)创建 100 条请求
+  it('模拟匀速(60ms/条)创建 100 条请求，预期执行不超过 100 次请求回调', function() {
+    this.timeout(6000);
     resetReadControlForTest();
+    let CaseReadNumber = 100;
+    let isDone = false;
+    let count = 0; //回调计数
     let i = 0;
-    let emitFunc = throttler(366);
     let timer = setInterval(function(){
-      if(i++ < 366){
+      if(i++ < CaseReadNumber){
         Cammand.execRead(UltrasonicProtocol, function(val){
-          emitFunc(function(){
+          if(++count == CaseReadNumber){
+            clearInterval(timer);
+            expect(count).to.eql(CaseReadNumber);
             done();
-          });
+          };
         });
       }else{
         clearInterval(timer);
+        expect(count).to.most(CaseReadNumber);
+        done();
       }
-    }, 10);
+    }, 60);
   });
 
   //模拟随机速度(10~100ms/条)创建 255 条请求
